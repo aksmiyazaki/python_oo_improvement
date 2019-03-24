b9faf8847b4b59a7d44ed8c4715bd7a077dbbec4
Alexandre Miyazaki <aksmiyazaki@gmail.com>
2019-03-24 17:20:17 -0300

    Advancing in assessment

---
+++ etl_python/code/address.py
@@ -0,0 +1,32 @@
+from os import chdir
+chdir('/home/aksmiyazaki/git/python_oo_improvement/etl_python/code')
+
+import pycountry_convert
+from geopy.location import Location
+
+class Address:
+    latitude = None
+    longitude = None
+    street = None
+    number = None
+    district = None
+    city = None
+    region = None
+    postal_code = None
+    country = None
+
+    def __init__(self, geopy_location):
+        try:
+            self.latitude = geopy_location.latitude
+            self.longitude = geopy_location.longitude
+            self.street = geopy_location.raw['Address'].split(geopy_location.raw['AddNum'])[0].strip()
+            self.number = geopy_location.raw['AddNum'].strip()
+            self.district = geopy_location.raw['District'].strip()
+            self.city = geopy_location.raw['City'].strip()
+            self.postal_code = geopy_location.raw['Postal'].strip()
+            self.region = geopy_location.raw['Region'].strip()
+            self.country = pycountry_convert.map_country_alpha3_to_country_name()[geopy_location.raw['CountryCode']].strip()
+        except Exception as e:
+            print(f"Error parsing object")
+            print(str(e))
+            print(f"{str(geopy_location.raw)}")
--- etl_python/code/pointlocator.py
+++ etl_python/code/pointlocator.py
@@ -1,12 +1,16 @@
+from os import chdir
+chdir('/home/aksmiyazaki/git/python_oo_improvement/etl_python/code')
+
 from geopy.location import Location
 from geopy.point import Point
 from geopy.geocoders import ArcGIS
+import address
 
 class PointLocator:
     __instance = None
     __cur_point = None
-    coord_decoder = None
-    decoded_point = None
+    __coord_decoder = None
+    __decoded_point = None
 
     @property
     def CurrentPoint(self):
@@ -22,22 +26,25 @@ class PointLocator:
             PointLocator()
         return PointLocator.__instance
 
-    def LocatePoint(self):
-        self.decoded_point = self.coord_decoder.reverse(self.__cur_point, exactly_one=True, timeout=300)
-
+    def locate_point(self):
+        self.__decoded_point = self.__coord_decoder.reverse(self.__cur_point, exactly_one=True, timeout=300)
 
     def __init__(self):
         if PointLocator.__instance is not None:
             raise Exception("PointLocator is a singleton class. Try GetInstance instead of constructing a new object.")
         else:
             PointLocator.__instance = self
-            self.coord_decoder = ArcGIS()
+            self.__coord_decoder = ArcGIS()
+
+    def get_located_point(self):
+        if self.__decoded_point is not None:
+            return self.__decoded_point
+        else:
+            return None
 
 p = PointLocator()
-pt = Point("30°02′59″S 51°12′05″W 2.2959 km")
-p.CurrentPoint = pt
-p.LocatePoint()
-p.decoded_point
-pt.latitude
-pt.longitude
-pt.altitude
+p.CurrentPoint = Point("30°02′59″S 51°12′05″W 2.2959 km")
+p.locate_point()
+lp = p.get_located_point()
+addr = address.Address(lp)
+lp.raw
